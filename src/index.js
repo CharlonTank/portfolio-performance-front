c557142746d212a874420cbc7903bc0ba22129d9
Charlon <charles.assus@gmail.com>
2020-01-13 19:21:18 +0100

    [WIP] Test to print process.env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 require("./styles.scss");
-
+// TEST
+console.log("TEST");
+console.log(process.env.FRONTEND_ENDPOINT);
 const {Elm} = require('./Main');
 var app = Elm.Main.init({
-    flags: {
-      configBackendEndpoint: process.env.BACKEND_ENDPOINT,
-	  configFrontendEndpoint: process.env.FRONTEND_ENDPOINT
-    }
+  flags: {
+    configBackendEndpoint: process.env.BACKEND_ENDPOINT,
+    configFrontendEndpoint: process.env.FRONTEND_ENDPOINT
+  }
 });
