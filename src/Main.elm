944492e825445c14d218efc170c9945258a96c65
Charlon <charles.assus@gmail.com>
2020-01-13 13:37:44 +0100

    Add box-sizing: border-box; to everything

--- src/Main.elm
+++ src/Main.elm
@@ -22,6 +22,7 @@ import Graphql.Http exposing (..)
 import Graphql.Operation exposing (RootMutation, RootQuery)
 import Graphql.OptionalArgument exposing (..)
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
+import Html
 import List.Extra exposing (..)
 import Modifiers exposing (..)
 import PortfolioPerformance.InputObject as InputObject
@@ -440,7 +441,7 @@ homeView model =
                     ]
                 ]
             ]
-            [ view model ]
+            [ ( Html.node "style" [] [ Html.text "*{box-sizing: border-box;}" ], [] ), view model ]
     }
 
 
