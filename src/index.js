62cc210cac92299c5cae9a2364bcc5e4ab84d258
Charlon <charles.assus@gmail.com>
2020-01-13 19:59:33 +0100

    Test show netlify env in console.log

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Elm } from './Main.elm'
-
+console.log("TEST : process.env.BACKEND_ENDPOINT");
+console.log(process.env.BACKEND_ENDPOINT);
 Elm.Main.init({
   node: document.querySelector('main')
 })
