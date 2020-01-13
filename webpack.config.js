f0034a09a0a78440914e2e8530e2ffdaec3b22b7
Charlon <charles.assus@gmail.com>
2020-01-13 19:08:24 +0100

    [WIP] Test add dotenv everywhere

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,9 @@ var common = {
         filename: MODE == "production" ? "[name]-[hash].js" : "index.js"
     },
     plugins: [
+        new Dotenv({
+            path: path.resolve(__dirname, './.env')
+        }),
         new HTMLWebpackPlugin({
             // Use this template to get basic responsive meta tags
             template: "src/index.html",
@@ -84,9 +87,6 @@ var common = {
 if (MODE === "development") {
     module.exports = merge(common, {
         plugins: [
-            new Dotenv({
-                path: path.resolve(__dirname, './.env')
-            }),
             // Suggested for hot-loading
             new webpack.NamedModulesPlugin(),
             // Prevents compilation errors causing the hot loader to lose state
